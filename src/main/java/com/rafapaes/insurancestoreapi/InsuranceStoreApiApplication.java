41ff8b2aef7e728f46ac70294b77dae385fcc2c9
RafaelMPaes <rafael.paes@sensedia.com>
2022-11-14 17:04:08 -0300

    Initial commit

---
+++ src/main/java/com/rafapaes/insurancestoreapi/InsuranceStoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.rafapaes.insurancestoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsuranceStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rafapaes/insurancestoreapi/InsuranceStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafapaes.insurancestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
